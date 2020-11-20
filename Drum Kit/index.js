8662387b7f87c997cd8a37fa0a9df150f364d9d4
sankalpjain99 <sankalpjain99@gmail.com>
2020-11-20 11:42:57 +0530

    Added Event Listeners

---
+++ Drum Kit/index.js
@@ -0,0 +1,11 @@
+
+var buttons = document.querySelectorAll(".drum");
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function(){
+        alert("I got Clicked");
+    });
+}
+
+
+
+
